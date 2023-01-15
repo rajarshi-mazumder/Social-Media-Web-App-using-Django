d9418f6d887e8f01dd1ce4bea034ccc539833b32
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2023-01-15 14:22:55 +0900

    Added bottom navbar, and made changes to matchmaking CSS

---
+++ PostIT/static/PostIT/js/bottom_navbar.js
@@ -0,0 +1,33 @@
+var is_right_sidebar_displayed = false;
+    window.onload = function () {
+
+        HideRightSidebar();
+        $('.profile-img-navbar').click(ToggleRightSidebar)
+        $('.blocker').click(ToggleRightSidebar);
+
+        $('.add-post-submenu').hide();
+        $('.profile-submenu').hide();
+    }
+    function ToggleRightSidebar() {
+        if (is_right_sidebar_displayed) {
+            HideRightSidebar();
+            is_right_sidebar_displayed = false;
+        }
+        else {
+            ShowRightSidebar();
+            is_right_sidebar_displayed = true;
+        }
+        console.log("Sidbar toggle!!", is_right_sidebar_displayed)
+    }
+    function HideRightSidebar() {
+        $('.sidebar-right-mobile').hide();
+        $('.blocker').hide();
+        console.log(" HideRightSidebar!!", is_right_sidebar_displayed)
+
+    }
+    function ShowRightSidebar() {
+        $('.sidebar-right-mobile').show();
+        $('.blocker').show();
+        console.log(" ShowRightSidebar!!", is_right_sidebar_displayed)
+
+    }
