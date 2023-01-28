9400d1aceb6d5ac95a3c49c5a2350469be500821
Rajarshi Mazumder <raj1156mazumder@gmail.com>
2023-01-28 18:48:55 +0900

    Updated mobile UI- profile page

--- PostIT/static/PostIT/js/bottom_navbar.js
+++ PostIT/static/PostIT/js/bottom_navbar.js
@@ -1,12 +1,16 @@
 var is_right_sidebar_displayed = false;
 window.onload = function () {
 
+    Default_Bottom_Navabar_State();
+}
+function Default_Bottom_Navabar_State(){
     HideRightSidebar();
     $('.profile-img-navbar').click(ToggleRightSidebar)
     $('.sidebar_right_blocker').click(ToggleRightSidebar);
 
     $('.add-post-submenu').hide();
     $('.profile-submenu').hide();
+    console.log("Conte!!")
 }
 function ToggleRightSidebar() {
     if (is_right_sidebar_displayed) {
